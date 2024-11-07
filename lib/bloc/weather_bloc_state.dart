5ea64d6b06c9c8ef4b5357cd453ec7a6f388d0be
Oscar Hernandez <oscarhernandezsoler@gmail.com>
2024-11-07 16:54:21 +0100

    design finished

--- lib/bloc/weather_bloc_state.dart
+++ lib/bloc/weather_bloc_state.dart
@@ -1,5 +1,10 @@
+// weather_bloc_state.dart
 part of 'weather_bloc_bloc.dart';
 
+import 'package:equatable/equatable.dart';
+import 'package:weather/weather.dart';  // Assuming you are using the 'weather' package for the Weather class
+
+// Abstract class for the weather states
 sealed class WeatherBlocState extends Equatable {
   const WeatherBlocState();
   
@@ -7,4 +12,21 @@ sealed class WeatherBlocState extends Equatable {
   List<Object> get props => [];
 }
 
+// Initial state (before weather data is fetched)
 final class WeatherBlocInitial extends WeatherBlocState {}
+
+// Loading state (when data is being fetched)
+final class WeatherBlocLoading extends WeatherBlocState {}
+
+// Failure state (when there is an error fetching the data)
+final class WeatherBlocFailure extends WeatherBlocState {}
+
+// Success state (when the weather data is successfully fetched)
+final class WeatherBlocSuccess extends WeatherBlocState {
+  final Weather weather;  // Weather data
+
+  const WeatherBlocSuccess(this.weather);
+
+  @override
+  List<Object> get props => [weather];
+}
---
+++ lib/data/my_data.dart
@@ -0,0 +1,2 @@
+
+String API_KEY = "1fe147957651d073e7d2efcfff9ea910";
